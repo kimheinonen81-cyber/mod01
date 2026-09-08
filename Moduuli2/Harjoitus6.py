58179f9803a3a882a8dac52628af289ffb558b83
kim heinonen <kimheinonen81@gmail.com>
2026-09-08 22:29:49 +0300

    Lisätty Moduuli2 Harjoitus6

---
+++ Moduuli2/Harjoitus6.py
@@ -0,0 +1,10 @@
+import random
+numero1 = random.randint(0, 9)
+numero2 = random.randint(0, 9)
+numero3 = random.randint(0, 9)
+print(f"Ensimmäinen koodi on {numero1}{numero2}{numero3}")
+numero4 = random.randint(1, 6)
+numero5 = random.randint(1, 6)
+numero6 = random.randint(1, 6)
+numero7 = random.randint(1, 6)
+print(f"Toinen koodi on {numero4}{numero5}{numero6}{numero7}")
